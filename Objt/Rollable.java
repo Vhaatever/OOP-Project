0dd864d2f6f929323c409dfc5b2c069382b219dd
Vhaatever <67260526+Vhaatever@users.noreply.github.com>
2021-11-10 14:09:04 +0530

    [WIP]Added super constructor for rollable sphere

--- Objt/Rollable.java
+++ Objt/Rollable.java
@@ -1,5 +1,14 @@
 package Objt;
 
 public class Rollable extends Objects{
-    
+   Rollable(float mass, float mu, float[] s0, float[] s, float[] u, float[] v, int objectID, int objectTypeID, float[] a)
+   {
+       super(mass, mu, s0, s, u, v, objectID, objectTypeID, a);
+   }
+
+public Rollable(float mass, float[] s0, float[] s, float[] u, float[] v, int objectID, int objectTypeID, float[] a)
+{
+    super(mass, s0, s, u, v, objectID, objectTypeID, a);
+    }
+
 }
--- Objt/Sphere.java
+++ Objt/Sphere.java
@@ -3,10 +3,15 @@ package Objt;
 public class Sphere extends Rollable {
     private float radius;
 
-    Sphere(float radius)
+    Sphere(float mass, float mu, float[] s0, float[] s, float[] u, float[] v, int objectID, int objectTypeID, float[] a, float radius) 
     {
-        
+        super(mass, mu, s0, s, u, v, objectID, objectTypeID, a);
         this.radius = radius;
+    }
+    
+    Sphere(float mass, float[] s0, float[] s, float[] u, float[] v, int objectID, int objectTypeID, float[] a, float radius) {
+        super(mass, s0, s, u, v, objectID, objectTypeID, a);
+        this.radius=radius;
     }
 
     public float getRadius() {
